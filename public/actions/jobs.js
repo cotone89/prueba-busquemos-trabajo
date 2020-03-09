876db9f6ba017b8c62a1b3411a2d8a1e12b4e45b
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-03-09 12:32:02 -0300

    se agregan estilos (faltan modales)

--- public/actions/jobs.js
+++ public/actions/jobs.js
@@ -2,14 +2,56 @@
 const base_url = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?';
 
 let form = document.getElementById('search_jobs');
+
+$(function () {
+    var selected = $('#tipoBusqueda').find(":selected").text();
+
+    $('#tipoBusqueda').on('change', function () {
+        let id = $(this).children(":selected").attr("id");
+        if (id == 1) {
+            $('#porDescripcion').show();
+            $('#porLugar').hide();
+            $('#porCoordenadas').hide();
+        } else if (id == 2) {
+            $('#porLugar').show();
+            $('#porDescripcion').hide();
+            $('#porCoordenadas').hide();
+        } else if (id == 3) {
+            $('#porCoordenadas').show();
+            $('#porDescripcion').hide();
+            $('#porLugar').hide();
+        }
+    });
+
+});
+
+
+
 form.addEventListener('submit', function (event) {
     event.preventDefault()
+
+    console.log("entro aqui");
+
+    $("#results").empty();
+   
 
     const description = document.querySelector('#description').value;
     const location = document.querySelector('#location').value;
     const lat = document.querySelector('#lat').value;
     const long = document.querySelector('#long').value;
-    const full_time = document.querySelector('#full_time').value;
+    let full_time;
+
+    console.log("la descripcion es-- " + description);
+
+
+    if ($("#si").is(':checked')) {
+        full_time = true;
+    } else {
+        full_time = false;
+    }
+
+
+    console.log(full_time);
 
     const data = {
         description,
@@ -19,6 +61,8 @@ form.addEventListener('submit', function (event) {
         full_time
     };
     obtieneJobs(data);
+
+
 })
 
 const obtieneJobs = async (data) => {
@@ -34,19 +78,45 @@ const obtieneJobs = async (data) => {
             }
         })
         if (resp.data) {
-            //data exist
-           // window.location.href = '/';
-            $("#results").append("<h2>Resultados de b&uacute;squeda</h2>");
+            /*$("#results").append("<h2>Resultados de b&uacute;squeda</h2>");
             $("#results").append("<br/>");
-            $("#results").append("<br/>");
+            $("#results").append("<br/>");*/
 
 
             $.each(resp.data, function (index, value) {
-               let hola = "<table><tr><td>"+value.id+"<td><tr></table >";
-                $("#results").append(hola);
+                let resultados =
+                    '<div class="col-md-4 pb-4">' +
+
+
+                    '<div class="d-flex flex-row border rounded carta-style">'+
+                        '<div class="p-0 w-25 align-self-center">'+
+                            '<img src="'+value.company_logo+'" alt="Imagen no disponible" class="img-thumbnail border-0" />'+
+
+                        '</div>'+
+                        '<div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">'+
+                            '<h4 class="text-primary">'+value.title+'</h4>'+
+                            '<h5 class="text-info">'+value.company+'</h5>'+
+                            '<ul class="m-0 color-li" style="list-style: none; margin:0; padding: 0">'+
+                                '<li><i class="fas fa-map-marker-alt"></i> '+value.location+'</li>'+
+                                '<li><i class="fab fa-twitter-square"></i> Twitter</li>'+
+                            '</ul>'+
+                            '<p class="text-right m-0"><!--<a href="#" class="btn btn-primary"><i class="far fa-user"></i> View Profile</a>--></p>'+
+                        '</div>'+
+                    '</div>'+
+
+
+
+                    '</div>';
+
+
+
+                $("#results").append(resultados);
+                $("#description").val("");
+                $("#location").val("");
+                $("#lat").val("");
+                $("#long").val("");
             });
 
-            //$("#results").append('<table><tr><td>'${resp.data}'<td><tr></table >');
         }
     } catch (e) {
         console.error(e);
